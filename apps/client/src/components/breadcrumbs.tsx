cfe9ed91fbf1bcee071b0c191a933c1f0e55578b
Xudong Huang <me@huangxudong.com>
2025-12-09 22:26:58 +0800

    fix: simplify Breadcrumbs component by removing unused props and optimizing breadcrumb generation

--- apps/client/src/components/breadcrumbs.tsx
+++ apps/client/src/components/breadcrumbs.tsx
@@ -1,9 +1,5 @@
 import { Fragment, useMemo } from "react";
-import {
-  LinkComponentProps,
-  useRouter,
-  useRouterState,
-} from "@tanstack/react-router";
+import { LinkComponentProps, useRouterState } from "@tanstack/react-router";
 import type { FC } from "react";
 import {
   Breadcrumb,
@@ -22,63 +18,51 @@ export interface BreadcrumbsItemProps {
   link: LinkComponentProps<"a">;
 }
 
-export interface BreadcrumbsProps {
-  baseItems?: BreadcrumbsItemProps[];
-}
-
-export const Breadcrumbs: FC<BreadcrumbsProps> = ({ baseItems = [] }) => {
-  const { buildLocation } = useRouter();
-
+export const Breadcrumbs: FC = () => {
   const matches = useRouterState({ select: (state) => state.matches });
 
-  const [breadcrumbs, lastBreadcrumb] = useMemo(() => {
-    const breadcrumbs = uniqBy(
-      [
-        ...baseItems.map((item) => ({
-          title: item.title,
-          path: buildLocation(item.link).pathname,
-        })),
-        ...matches
-          .map(({ pathname, context }) => {
-            if (!context.title) {
-              return null;
-            }
+  const breadcrumbs = useMemo(() => {
+    return uniqBy(
+      matches
+        .map(({ pathname, context }) => {
+          if (!context.title) {
+            return null;
+          }
 
-            return {
-              title: context.title,
-              path: pathname,
-            };
-          })
-          .filter((item) => item !== null),
-      ],
+          return {
+            title: context.title,
+            path: pathname,
+          };
+        })
+        .filter((item) => item !== null),
       (item) => item.path.replace(/\/+$/, ""),
     );
-
-    const lastBreadcrumb = breadcrumbs.pop();
-
-    return [breadcrumbs, lastBreadcrumb];
   }, [matches]);
 
   return (
     <Breadcrumb>
       <BreadcrumbList>
-        {breadcrumbs?.map((breadcrumb) => (
-          <Fragment key={breadcrumb.title}>
-            <BreadcrumbItem className="hidden md:block">
-              <BreadcrumbLink asChild>
-                <Link to={breadcrumb.path}>{breadcrumb.title}</Link>
-              </BreadcrumbLink>
-            </BreadcrumbItem>
+        {breadcrumbs.map((breadcrumb, index) => {
+          if (index === breadcrumbs.length - 1) {
+            return (
+              <BreadcrumbItem key={breadcrumb.path}>
+                <BreadcrumbPage>{breadcrumb.title}</BreadcrumbPage>
+              </BreadcrumbItem>
+            );
+          }
 
-            <BreadcrumbSeparator className="hidden md:block" />
-          </Fragment>
-        ))}
+          return (
+            <Fragment key={breadcrumb.path}>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink asChild>
+                  <Link to={breadcrumb.path}>{breadcrumb.title}</Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
 
-        {lastBreadcrumb && (
-          <BreadcrumbItem>
-            <BreadcrumbPage>{lastBreadcrumb.title}</BreadcrumbPage>
-          </BreadcrumbItem>
-        )}
+              <BreadcrumbSeparator className="hidden md:block" />
+            </Fragment>
+          );
+        })}
       </BreadcrumbList>
     </Breadcrumb>
   );
