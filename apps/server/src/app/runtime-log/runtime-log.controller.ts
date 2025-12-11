05c3718971a2c54384a96aaa911a02a32941a6ac
Xudong Huang <me@huangxudong.com>
2025-12-11 15:40:48 +0800

    feat: enhance runtime log streaming by committing transactions to prevent long delays

--- apps/server/src/app/runtime-log/runtime-log.controller.ts
+++ apps/server/src/app/runtime-log/runtime-log.controller.ts
@@ -1,20 +1,32 @@
+import { EntityManager } from '@mikro-orm/postgresql';
 import { Controller, MessageEvent, Param, Query, Sse } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { finalize, Observable } from 'rxjs';
 
 import { RuntimeLogService } from './runtime-log.service';
 
 @Controller('api/services')
 export class RuntimeLogController {
-  constructor(private readonly runtimeLogService: RuntimeLogService) {}
+  constructor(
+    private readonly runtimeLogService: RuntimeLogService,
+    private readonly em: EntityManager,
+  ) {}
 
   @Sse(':serviceId/runtime-logs')
   async getRuntimeLogs(
     @Param('serviceId') serviceId: string,
     @Query('tailLines') tailLines?: string,
   ): Promise<Observable<MessageEvent>> {
-    return await this.runtimeLogService.streamLogs(
+    const observable = await this.runtimeLogService.streamLogs(
       serviceId,
       tailLines ? parseInt(tailLines, 10) : 500,
     );
+
+    // 如果在事务中，则提交事务。
+    // 防止长时间流式获取日志导致事务一直不提交。
+    if (this.em.isInTransaction()) {
+      await this.em.commit();
+    }
+
+    return observable;
   }
 }
