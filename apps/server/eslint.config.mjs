782c1c9f16298aebe7b25763069730fbaa8fbf56
Xudong Huang <me@huangxudong.com>
2026-01-20 18:06:12 +0800

    fix: simplify ESLint configuration by consolidating imports and removing unnecessary parser options

--- apps/server/eslint.config.mjs
+++ apps/server/eslint.config.mjs
@@ -1,27 +1,11 @@
-import config from '@nest-boot/eslint-config';
-import tsParser from '@typescript-eslint/parser';
-
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import baseConfig from '@nest-boot/eslint-config';
 
 /** @type {import('eslint').Linter.Config[]} */
 const config = [
   {
     ignores: ['eslint.config.mjs', 'src/database/migrations/**'],
   },
-  ...config,
-  {
-    languageOptions: {
-      /** @type {import('eslint').Linter.Parser} */
-      parser: tsParser,
-      parserOptions: {
-        project: resolve(__dirname, 'tsconfig.json'),
-      },
-    },
-  },
+  ...baseConfig,
   {
     rules: {
       '@typescript-eslint/restrict-template-expressions': [
